80826c4aa3a0a85f44001aebe13afaf98db2fb00
Seyed Mostafa Hashemian <shashemi@c2r1s5.42madrid.com>
2024-08-23 09:19:13 +0200

    a

---
+++ ex08/ft_sort_int_tab.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shashemi <shashemi@student.42maddrid.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/23 09:06:57 by shashemi          #+#    #+#             */
+/*   Updated: 2024/08/23 09:06:59 by shashemi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	aux;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (tab[i] > tab[j])
+			{
+				aux = tab[i];
+				tab[i] = tab[j];
+				tab[j] = aux;
+			}
+			j++;
+		}
+		i++;
+	}
+}
